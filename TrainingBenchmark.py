2f7c81f2facbd46836cd6202bcb99cfe009adac2
koolo233 <koolo233@163.com>
2023-03-03 20:25:07 +0800

    Update TrainingBenchmark.py

--- TrainingBenchmark.py
+++ TrainingBenchmark.py
@@ -48,7 +48,7 @@ model_list = {
 
 parser = argparse.ArgumentParser(description='Params of Benchmark')
 
-parser.add_argument("-m", "--model_type", type=str, default="AlexNet", choices=model_list.keys())
+parser.add_argument("-m", "--model_type", type=str, default="ResNet50", choices=model_list.keys())
 parser.add_argument("-b", "--batch_size", type=int, default=64)
 parser.add_argument("-d", "--dataset_name", type=str, default="CIFAR10", choices=["CIFAR10"])
 
@@ -124,6 +124,7 @@ if __name__ == "__main__":
         device_name_str += f"_{p.name}"
     random_str = generate_random_str()
     log_file_name = "./log/{}_{}{}_{}_{}.txt".format(args.model_type, args.dataset_name, device_name_str, args.batch_size, random_str)
+    with open(log_file_name, "a") as f: f.write(str(time.time()) + "\n")
 
     # optimizing
     criterion = nn.CrossEntropyLoss()
@@ -191,7 +192,7 @@ if __name__ == "__main__":
     test_tensor = torch.rand([1, 3, 224, 224]).to(device)
     flops = FlopCountAnalysis(net, test_tensor).total()
     params = parameter_count(net)[""]
-    with open(log_file_name, "w") as f:
+    with open(log_file_name, "a") as f:
         log_str = "PARAMS: {:.4f}MB, FLOPS: {:.4f}MB\n".format(params / (1024 ** 2), flops / (1024 ** 2))
         print(log_str)
         f.write(log_str)
